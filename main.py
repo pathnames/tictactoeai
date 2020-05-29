c98bba050c3a9053e3675b9f1a7fd676ee798fb3
pjkpjk900 <pk58@buffalo.edu>
2020-05-29 11:08:27 -0400

    Ability to make moves added

--- main.py
+++ main.py
@@ -15,5 +15,28 @@ def render(board):
                 print(str(board[i][j]) + " ", end = "") 
              
         print()
-       
-render(new_board()) 
+
+def get_move():
+    print("Which number would you like to place on?")
+    num = int(input())
+    if num <= 0 or num > 9:
+        print("Invalid input!")
+        get_move()
+    else:
+        move_dict = {1:[0, 0], 2: [0, 1], 3: [0, 2], 4: [1, 0], 5: [1, 1], 6:[1, 2], 7:[2, 0], 8: [2, 1], 9: [2, 2]}
+        return move_dict[num]
+
+def make_move(board, move_coordinates, player):
+   temp = new_board()
+   for i in range(3):
+       for j in range(3):
+           temp[i][j] = board[i][j]
+   temp[move_coordinates[0]][move_coordinates[1]] = player
+   return temp
+
+board = new_board()
+render(board) 
+move1 = get_move()
+print(move1)
+board = make_move(board, move1, 'X')
+render(board)
